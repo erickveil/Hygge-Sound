a9ae59079bb2411e756ca9b91c4adec01b1021a8
eveil <erick@erickveil.net>
2024-07-06 08:46:47 -0700

    Refactored the View to work a bit better

--- app/src/main/java/net/erickveil/calmsound/MainActivity.kt
+++ app/src/main/java/net/erickveil/calmsound/MainActivity.kt
@@ -1,8 +1,5 @@
 package net.erickveil.calmsound
 
-import android.media.AudioFormat
-import android.media.AudioManager
-import android.media.AudioTrack
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -11,12 +8,10 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import net.erickveil.calmsound.viewmodel.BrownNoiseViewModel
-import kotlin.random.Random
-
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import net.erickveil.calmsound.intent.BrownNoiseIntent
 import net.erickveil.calmsound.model.BrownNoiseState
-
+import net.erickveil.calmsound.ui.theme.CalmSoundTheme
 
 class MainActivity : ComponentActivity() {
 
@@ -25,22 +20,20 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val state by viewModel.state.collectAsStateWithLifecycle()
-            BrownNoisePlayerScreen(
-                state = state,
-                onTogglePlayback = {
-                    viewModel.processIntent(BrownNoiseIntent.TogglePlayback)
-                }
-
-            )
+            CalmSoundTheme {
+                val state by viewModel.state.collectAsStateWithLifecycle()
+                BrownNoisePlayerScreen(
+                    state = state,
+                    onTogglePlayback = {
+                        viewModel.processIntent(BrownNoiseIntent.TogglePlayback)
+                    }
+                )
+            }
         }
     }
 
     @Composable
-    fun BrownNoisePlayerScreen(
-        state: BrownNoiseState,
-        onTogglePlayback: () -> Unit
-    ) {
+    fun BrownNoisePlayerScreen( state: BrownNoiseState, onTogglePlayback: () -> Unit ) {
         Button(onClick = onTogglePlayback) {
             Text(if (state.isPlaying) "Stop Brown Noise" else "Play Brown Noise")
         }
