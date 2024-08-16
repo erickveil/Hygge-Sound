4bd7487617411415e5ae5d6a1b8b71f7f79e3cf2
Erick Veil <erick@erickveil.net>
2024-08-16 12:28:20 -0700

    Added ios directory and base project

---
+++ ios/Hygge-Sound/Hygge_SoundApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hygge_SoundApp.swift
+//  Hygge-Sound
+//
+//  Created by Erick Veil on 8/16/24.
+//
+
+import SwiftUI
+
+@main
+struct Hygge_SoundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
