f0e6374c7f1941b10f2e7dd76d6cef87a8ede0ce
Erick Veil <erick@erickveil.net>
2024-08-22 12:58:44 -0700

    Changed look and feel to match Android version

--- ios/Hygge-Sound/ContentView.swift
+++ ios/Hygge-Sound/ContentView.swift
@@ -16,20 +16,36 @@ struct ContentView: View {
     @State private var isPlaying = false
     
     var body: some View {
-        Button(action: {
-            if self.isPlaying {
-                self.brownNoisePlayer.stop()
-            } else {
-                self.brownNoisePlayer.play()
-            }
-            self.isPlaying.toggle()
-        }) {
-            Text(self.isPlaying ? "Stop Calm Sound" : "Play Calm Sound")
-                .font(.largeTitle)
-                .foregroundColor(.white)
+        ZStack {
+            Color.black.edgesIgnoringSafeArea(.all)
+            
+            Button(action: {
+                if self.isPlaying {
+                    self.brownNoisePlayer.stop()
+                } else {
+                    self.brownNoisePlayer.play()
+                }
+                self.isPlaying.toggle()
+            }) {
+                VStack {
+                    Text(self.isPlaying ? "Stop Calm Sound" : "Play Calm Sound")
+                        .font(.system(size: 24))
+                        .fontWeight(.medium)
+                        .foregroundColor(.black)
+                        .padding(.bottom, 10)
+                    Image("csicon")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 125, height: 125)
+                }
+                .padding()
                 .frame(maxWidth: .infinity, maxHeight: .infinity)
-                .background(Color.blue)
+                .background(Color(red: 245/255, green: 245/255, blue: 220/255))
                 .edgesIgnoringSafeArea(.all)
+                .cornerRadius(90)
+                .padding(10)
+
+            }
         }
     }
 }
